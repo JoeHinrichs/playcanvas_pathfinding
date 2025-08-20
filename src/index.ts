2415074123832a0a5de09200d096f9e85d6f0b53
JoeHinrichs <joe@jhinrichs.com>
2025-08-20 09:03:42 -0500

    generate navmesh

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import * as pc from 'playcanvas';
-import { init } from 'recast-navigation';
-import { pcToSoloNavMesh, pcToTiledNavMesh, pcToTileCache } from 'recast-navigation-playcanvas';
+import { init } from '@recast-navigation/core';
+import { pcToSoloNavMesh } from '@recast-navigation/playcanvas';
 import { loadScene } from './modules/utilities';
 import { CameraControls } from './modules/camera-controls.mjs';
 
@@ -14,31 +14,51 @@ const app = new pc.Application(canvas);
 app.setCanvasResolution(pc.RESOLUTION_AUTO);
 app.setCanvasFillMode(pc.FILLMODE_FILL_WINDOW);
 
-initRecast();
 loadScene(app, "config.json", "2300273.json", preloadProgress, startApplication);
-
-async function initRecast() {
-  await init()
-  console.log("Recast initialized");
-}
 
 function preloadProgress(value: number) {
     console.log(`Preload progress: ${value * 100}%`);
 }
 
-function startApplication() {
+async function startApplication() {
     camera = app.root.findByName("Camera") as pc.Entity;
     if (camera?.script) {
         const cc = (camera.script.create(CameraControls) as any); //PlayCanvas types are not fully compatible with TypeScript
         if (cc) {
             cc.enableFly = false;
-            cc.enablePan = false; 
+            cc.enablePan = false;
             cc.rotateSpeed = 0.5;
             cc.zoomSpeed = 0.005;
             //cc._startZoomDist = 30;
             //cc.focus(new pc.Vec3(0, 0, 0), true);
-         }
+        }
     }
+    await init(); // ensure Recast is initialized before generating the navmesh
+    
+    /* generate a solo navmesh */
+    const platform = app.root.findByName("platform") as pc.Entity;
+    if (platform?.render?.meshInstances) {
+        const meshInstances = platform.render.meshInstances;
+        console.log("Mesh instances:", meshInstances);
+        const { success, navMesh } = pcToSoloNavMesh(meshInstances, {
+            //cellSize: 0.3,
+            //cellHeight: 0.2,
+            //agentHeight: 2.0,
+            //agentRadius: 0.6,
+            //agentMaxClimb: 0.9,
+            //agentMaxSlope: 45.0,
+            //regionMinSize: 8,
+            //regionMergeSize: 20,
+            //edgeMaxLen: 12.0,
+            //edgeMaxError: 1.3,
+            //vertsPerPoly: 6,
+            //detailSampleDist: 6.0,
+            //detailSampleMaxError: 1.0
+        });
+        console.log("Navmesh generation success:", success);
+        console.log("Navmesh:", navMesh);
+    }
+    //const { success, navMesh } = pcToSoloNavMesh(meshInstances, {});
 }
 
 
