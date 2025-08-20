267b5cc327f4c425e0b0fe85807632bc4d39506f
JoeHinrichs <joe@jhinrichs.com>
2025-08-20 08:46:59 -0500

    camera positioning

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import * as pc from 'playcanvas';
+import { init } from 'recast-navigation';
+import { pcToSoloNavMesh, pcToTiledNavMesh, pcToTileCache } from 'recast-navigation-playcanvas';
 import { loadScene } from './modules/utilities';
 import { CameraControls } from './modules/camera-controls.mjs';
+
 import './index.css';
 
 let camera: pc.Entity | null;
@@ -11,7 +14,13 @@ const app = new pc.Application(canvas);
 app.setCanvasResolution(pc.RESOLUTION_AUTO);
 app.setCanvasFillMode(pc.FILLMODE_FILL_WINDOW);
 
+initRecast();
 loadScene(app, "config.json", "2300273.json", preloadProgress, startApplication);
+
+async function initRecast() {
+  await init()
+  console.log("Recast initialized");
+}
 
 function preloadProgress(value: number) {
     console.log(`Preload progress: ${value * 100}%`);
@@ -20,15 +29,16 @@ function preloadProgress(value: number) {
 function startApplication() {
     camera = app.root.findByName("Camera") as pc.Entity;
     if (camera?.script) {
-        const cc = (camera.script.create(CameraControls) as any);
+        const cc = (camera.script.create(CameraControls) as any); //PlayCanvas types are not fully compatible with TypeScript
         if (cc) {
             cc.enableFly = false;
             cc.enablePan = false; 
             cc.rotateSpeed = 0.5;
             cc.zoomSpeed = 0.005;
+            //cc._startZoomDist = 30;
+            //cc.focus(new pc.Vec3(0, 0, 0), true);
          }
     }
-
 }
 
 
