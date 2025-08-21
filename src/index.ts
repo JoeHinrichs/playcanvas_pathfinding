54496325d84e413124822ee2d8abf12f61d03157
JoeHinrichs <joe@jhinrichs.com>
2025-08-21 10:34:35 -0500

    add Ammo

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,42 @@
 import * as pc from 'playcanvas';
 import { init as recastInit } from '@recast-navigation/core';
 import { loadScene } from './modules/utilities';
-import { Platform } from './scripts/platform.mjs';
 import { CameraControls } from './modules/camera-controls.mjs';
+import { Platform } from './scripts/platform.mjs';
 import './index.css';
 
 let camera: pc.Entity | null;
 
 // create an application
 const canvas = document.getElementById('application') as HTMLCanvasElement;
-const app = new pc.Application(canvas);
+const app = new pc.Application(canvas, {
+    mouse: new pc.Mouse(canvas),
+    touch: new pc.TouchDevice(canvas),
+    keyboard: new pc.Keyboard(window),
+    elementInput: new pc.ElementInput(canvas)
+});
 app.setCanvasResolution(pc.RESOLUTION_AUTO);
 app.setCanvasFillMode(pc.FILLMODE_FILL_WINDOW);
 
-loadScene(app, "config.json", "2300273.json", preloadProgress, startApplication);
+//install ammo physics
+loadAmmo().then(() => {
+    console.log("Ammo physics loaded successfully");
+    loadScene(app, "config.json", "2300273.json", preloadProgress, startApplication);
+}).catch((error) => {
+    console.error("Failed to load Ammo physics:", error);
+});
+
+async function loadAmmo() {
+    pc.WasmModule.setConfig('Ammo', {
+        glueUrl: './files/assets/246434436/1/ammo.wasm.js',
+        wasmUrl: './files/assets/246434437/1/ammo.wasm.wasm',
+        fallbackUrl: './files/assets/246434435/1/ammo.j'
+    });
+
+    await new Promise((resolve: Function) => {
+        pc.WasmModule.getInstance('Ammo', () => resolve());
+    });
+}
 
 function preloadProgress(value: number) {
     console.log(`Preload progress: ${value * 100}%`);
@@ -35,10 +58,14 @@ async function startApplication() {
 
     await recastInit(); // ensure Recast is initialized before generating the navmesh   
 
-    /* generate a solo navmesh */
+    /* add platform */
     const platform = app.root.findByName("platform") as pc.Entity;
     platform.addComponent('script');
-    if(platform?.script) platform.script.create(Platform);
+    if (platform?.script) platform.script.create(Platform);
+
+
+
+
 }
 
 
--- src/scripts/platform.mjs
+++ src/scripts/platform.mjs
@@ -2,14 +2,18 @@ import * as pc from 'playcanvas';
 import { Crowd, NavMeshQuery } from '@recast-navigation/core';
 import { pcToSoloNavMesh, NavMeshHelper } from '@recast-navigation/playcanvas';
 
-let crowd, query = null;
+let crowd, query, cameraEntity = null;
 
 export class Platform extends pc.Script {
     static scriptName = 'platform';
+    static ray = new pc.Ray();
+    static hitPosition = new pc.Vec3();
+    static newPosition = new pc.Vec3();
 
     initialize() {
 
         const wall = this.app.root.findByName("wall");
+        cameraEntity = this.app.root.findByName("Camera");
 
         if (this.entity.render?.meshInstances && wall?.render?.meshInstances) {
             let meshInstances = this.entity.render.meshInstances.concat(wall.render.meshInstances);
@@ -50,12 +54,53 @@ export class Platform extends pc.Script {
                 const targetPosition = { x: -8, y: 1, z: 0 };
                 heroAgent.requestMoveTarget(targetPosition);
             }
+
+            pc.app.mouse.on(pc.EVENT_MOUSEDOWN, this.onMouseDown, this);
+
+            if (pc.app.touch) pc.app.touch.on(pc.EVENT_TOUCHSTART, this.onTouchStart, this);
+
+            this.on('destroy', () => {
+                pc.app.mouse.off(pc.EVENT_MOUSEDOWN, this.onMouseDown, this);
+
+                if (pc.app.touch) {
+                    pc.app.touch.off(pc.EVENT_TOUCHSTART, this.onTouchStart, this);
+                }
+            });
         }
+    }
+
+    onMouseDown = (event) => {
+        if (event.button === pc.MOUSEBUTTON_LEFT) {
+            this.doRayCast(event);
+        }
+    };
+
+    onTouchStart = (event) => {
+        if (event.touches.length === 1) {
+            this.doRayCast(event.touches[0]);
+            event.event.preventDefault();
+        }
+    };
+
+    doRayCast(screenPosition) {
+        const ray = Platform.ray;
+        const hitPosition = Platform.hitPosition;
+        const camera = cameraEntity.camera;
+
+        camera.screenToWorld(screenPosition.x, screenPosition.y, camera.nearClip, ray.origin);
+        camera.screenToWorld(screenPosition.x, screenPosition.y, camera.farClip, ray.direction);
+        ray.direction.sub(ray.origin).normalize();
+
+        /*const hit = this.groundShape.intersectsRay(ray, hitPosition);
+        if (hit) {
+            console.log("Hit position:", hitPosition);
+            //this.movePlayerTo(hitPosition);
+        }*/
     }
 
     update(dt) {
         if (crowd) {
-            crowd.update(1/60, dt, 10);
+            crowd.update(1 / 60, dt, 10);
             const hero = this.app.root.findByName("hero");
             const heroAgent = crowd.agents[0];
             if (hero && heroAgent) {
